b96e3f9ff3c47783d81759052daa805fb885c07f
Victor <vik.0n@yandex.ru>
2023-03-15 03:37:21 +0300

    new tags and check len of tensor added

--- parse_proc/split_page.py
+++ parse_proc/split_page.py
@@ -2,9 +2,12 @@ from bs4 import BeautifulSoup
 from bs4.element import Tag
 from transformers import AutoTokenizer
 
+maxNum_chars = 2250
+maxLen_tensor = 512
+
 tokenizer = AutoTokenizer.from_pretrained("cointegrated/LaBSE-en-ru")
 
-blocks = ["p", "h1", "h2", "h3", "h4", "h5"]
+blocks = ["ol", "ul", "tr", "p", "h1", "h2", "h3", "h4", "h5"]
 
 
 def _extract_blocks(parent_tag):
@@ -24,22 +27,27 @@ def _extract_blocks(parent_tag):
 def to_blocktext(html_text):
     soup = BeautifulSoup(html_text, features="lxml")
     extracted_blocks = _extract_blocks(soup.body)
-    max_len_of_blocks = max([len(x) for x in [block.get_text().strip() for block in extracted_blocks]])
+    encoded_text_blocks = [block.get_text().strip() for block in extracted_blocks]
+    encoded_blocks = tokenizer(encoded_text_blocks, padding=True, max_length=None, return_tensors='pt')
+    max_len_of_blocks = len(encoded_blocks['input_ids'][0])
     extracted_blocks_texts = []
     text_block = ""
     k = 0
 
     for i in range(len(extracted_blocks) - 1):
-        if not text_block and k < 2250 // max_len_of_blocks:
+        if not text_block or k < maxLen_tensor // max_len_of_blocks:
             text_block += " " + extracted_blocks[i].get_text().strip()
             k += 1
             continue
-        if len(text_block + " " + extracted_blocks[i + 1].get_text().strip()) > 2250:
+        if tokenizer(text_block + " " + extracted_blocks[i + 1].get_text().strip(), padding=True, max_length=None,
+                     return_tensors='pt')['input_ids'].shape[-1] > maxLen_tensor:
             extracted_blocks_texts.append(text_block)
             text_block = ""
             k = 0
 
         text_block += " " + extracted_blocks[i + 1].get_text().strip()
+
+    extracted_blocks_texts.append(text_block)
 
     if extracted_blocks[-1].get_text().strip() not in extracted_blocks_texts[-1]:
         extracted_blocks_texts.append(extracted_blocks[-1].get_text().strip())
@@ -47,15 +55,10 @@ def to_blocktext(html_text):
     return extracted_blocks_texts
 
 
-txt = open('3.html', 'r', encoding="utf8").read()
-
-soup = BeautifulSoup(txt, 'lxml')
+with open('3.html', 'r', encoding="utf8") as f:
+    txt = f.read()
 
 list_of_blocks = to_blocktext(txt)
 list_of_lens = [len(x) for x in list_of_blocks]
-print(list_of_blocks)
-print(len(list_of_blocks))
 
-encoded_input = tokenizer(list_of_blocks, padding=True, truncation=True, max_length=64, return_tensors='pt')
-
-print(encoded_input['input_ids'])
+encoded_input = tokenizer(list_of_blocks, padding=True, max_length=None, return_tensors='pt')
