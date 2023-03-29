c29e590bbda27d1a7af873fd875b0fb1be9f882e
Victor <vik.0n@yandex.ru>
2023-03-29 22:58:18 +0300

    added grouping by h* tags

--- parse_proc/split_page.py
+++ parse_proc/split_page.py
@@ -1,20 +1,20 @@
 from bs4 import BeautifulSoup
 from bs4.element import Tag
+
 from transformers import AutoTokenizer
 
-maxNum_chars = 2250
 maxLen_tensor = 512
 
 tokenizer = AutoTokenizer.from_pretrained("cointegrated/LaBSE-en-ru")
 
-blocks = ["ol", "ul", "tr", "p", "h1", "h2", "h3", "h4", "h5"]
+blocks = ["span", "li", "a", "ol", "tr", "p", "h1", "h2", "h3", "h4", "h5"]
 
 
 def _extract_blocks(parent_tag):
     extracted_blocks = []
     for tag in parent_tag:
         if tag.name in blocks:
-            extracted_blocks.append(tag)
+            extracted_blocks.append([tag, tag.name])
             continue
         if isinstance(tag, Tag):
             if len(tag.contents) > 0:
@@ -24,41 +24,63 @@ def _extract_blocks(parent_tag):
     return extracted_blocks
 
 
+# сгруппируем текст по заголовкам
+def sum_block_h(blocks):
+    h_blocks = []
+    txt_block = ""
+    activate = False
+    for block in blocks:
+        if not activate and 'h' in block[1]:
+            activate = True
+        if activate and 'h' in block[1]:
+            h_blocks.append(txt_block)
+            txt_block = ""
+        elif tokenizer(txt_block + " " + block[0].get_text().strip(), padding=True, max_length=None,
+                       return_tensors='pt')['input_ids'].shape[-1] > maxLen_tensor:
+            h_blocks.append(txt_block)
+            txt_block = ""
+        txt_block += " " + block[0].get_text().strip()
+    if txt_block:
+        h_blocks.append(txt_block)
+    return h_blocks
+
+
+# функция, которая получает на вход страницу html и возвращает список блоков
 def to_blocktext(html_text):
     soup = BeautifulSoup(html_text, features="lxml")
     extracted_blocks = _extract_blocks(soup.body)
-    encoded_text_blocks = [block.get_text().strip() for block in extracted_blocks]
-    encoded_blocks = tokenizer(encoded_text_blocks, padding=True, max_length=None, return_tensors='pt')
-    max_len_of_blocks = len(encoded_blocks['input_ids'][0])
+
+    grouped_text = sum_block_h(extracted_blocks)
     extracted_blocks_texts = []
     text_block = ""
-    k = 0
 
-    for i in range(len(extracted_blocks) - 1):
-        if not text_block or k < maxLen_tensor // max_len_of_blocks:
-            text_block += " " + extracted_blocks[i].get_text().strip()
-            k += 1
-            continue
-        if tokenizer(text_block + " " + extracted_blocks[i + 1].get_text().strip(), padding=True, max_length=None,
+    for i in range(len(grouped_text)):
+        if tokenizer(text_block + grouped_text[i], padding=True, max_length=None,
                      return_tensors='pt')['input_ids'].shape[-1] > maxLen_tensor:
             extracted_blocks_texts.append(text_block)
             text_block = ""
-            k = 0
 
-        text_block += " " + extracted_blocks[i + 1].get_text().strip()
+        text_block += grouped_text[i]
 
-    extracted_blocks_texts.append(text_block)
+    if text_block:
+        extracted_blocks_texts.append(text_block)
 
-    if extracted_blocks[-1].get_text().strip() not in extracted_blocks_texts[-1]:
-        extracted_blocks_texts.append(extracted_blocks[-1].get_text().strip())
+    if grouped_text[-1] not in extracted_blocks_texts[-1]:
+        extracted_blocks_texts.append(text_block)
 
     return extracted_blocks_texts
 
 
-with open('3.html', 'r', encoding="utf8") as f:
-    txt = f.read()
+def main():
+    path_to_html = ''  # путь к странице html
+    if not path_to_html:
+        print("Пропиши путь к файлу")
+        return
+    with open(path_to_html, 'r', encoding="utf8") as f:
+        txt = f.read()
 
-list_of_blocks = to_blocktext(txt)
-list_of_lens = [len(x) for x in list_of_blocks]
+    list_of_blocks = to_blocktext(txt)
 
-encoded_input = tokenizer(list_of_blocks, padding=True, max_length=None, return_tensors='pt')
+
+if __name__ == "__main__":
+    main()
